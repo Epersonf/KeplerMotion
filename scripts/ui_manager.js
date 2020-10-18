b37a185e39afc17655316c653f034e4da33f6423
EPERSON <epersoncmxf@gmail.com>
2020-10-17 21:28:05 -0300

    Areas amount

--- scripts/ui_manager.js
+++ scripts/ui_manager.js
@@ -1,18 +1,20 @@
 const canvas = document.getElementById("canvas");
 const a_input = new Input_Value_Default(document.getElementById("a"));
 const b_input = new Input_Value_Default(document.getElementById("b"));
+const areas_amt_input = new Input_Value_Default(document.getElementById("areas-amt"));
 const use_left = new Input_Value_CheckBox(document.getElementById("use-left"));
 const ctx = canvas.getContext('2d');
 const ellipse = new Ellipse(100, 200, true, ctx, "#fafafa");
 
 a_input.onChange(updateSize);
 b_input.onChange(updateSize);
+areas_amt_input.onChange(updateSize);
 use_left.onChange(updateSize);
 
 function updateSize() {
     clearCanvas();
     ellipse.setSize(a_input.getFloat(), b_input.getFloat(), use_left.getBool());
-    ellipse.draw(canvas.width/2, canvas.height/2);
+    ellipse.draw(canvas.width/2, canvas.height/2, areas_amt_input.getInt());
 }
 
 function clearCanvas() {
